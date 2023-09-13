8bf09891e96858c6eb9e30439cd05286d8d31ea4
Ajibade <ajibadeemmanuel58@gmail.com>
2023-09-13 20:54:39 +0100

    cors error

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 app.use(cors({ origin: "https://moskol.vercel.app" }));
+app.use(cors({ origin: "http://localhost:3000" }));
 
 app.use(express.static("public"));
 
